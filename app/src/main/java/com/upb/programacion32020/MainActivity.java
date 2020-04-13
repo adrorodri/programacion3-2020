753d39f7a6690016be0f780ba3f5bfdaec940280
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2020-04-13 09:18:37 -0400

    UI Elements added

--- app/src/main/java/com/upb/programacion32020/MainActivity.java
+++ app/src/main/java/com/upb/programacion32020/MainActivity.java
@@ -3,12 +3,95 @@ package com.upb.programacion32020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Switch adminSwitch;
+    EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ui_elements);
+
+        button = findViewById(R.id.loginButton);
+        adminSwitch = findViewById(R.id.switchAdmin);
+        editTextPassword = findViewById(R.id.passwordEditText);
+
+        button.setText("OTRA COSA");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TEST", "TEST BOTON");
+            }
+        });
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Log.d("TEST", "TEST BOTON LONG");
+                return true;
+            }
+        });
+
+        adminSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if(checked) {
+                    Log.d("TEST", "SWITCH -> TRUE");
+                } else {
+                    Log.d("TEST", "SWITCH -> FALSE");
+                }
+            }
+        });
+
+        editTextPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int action, KeyEvent keyEvent) {
+                if (action == EditorInfo.IME_ACTION_SEARCH) {
+                    Log.d("TEST", "KEYBOARD DONE");
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
