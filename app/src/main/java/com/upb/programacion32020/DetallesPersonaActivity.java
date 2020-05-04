40515bd02f35db97d80e7ce962aab8e7f7df0ed1
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2020-05-04 09:20:43 -0400

    Share Text as implicit intent

--- app/src/main/java/com/upb/programacion32020/DetallesPersonaActivity.java
+++ app/src/main/java/com/upb/programacion32020/DetallesPersonaActivity.java
@@ -2,6 +2,8 @@ package com.upb.programacion32020;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class DetallesPersonaActivity extends AppCompatActivity {
 
     ImageView imageViewPersona;
+    Button buttonShare;
+    Persona persona;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,23 @@ public class DetallesPersonaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detalles_persona);
 
         imageViewPersona = findViewById(R.id.ivPersona);
+        buttonShare = findViewById(R.id.btShare);
 
         Intent intent = getIntent();
         if (intent.hasExtra("persona")) {
-            Persona persona = (Persona) intent.getSerializableExtra("persona");
+            persona = (Persona) intent.getSerializableExtra("persona");
             imageViewPersona.setImageResource(persona.getImage());
         }
+
+        buttonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "Hey, comparte la aplicacion con " + persona.getNombre());
+                startActivity(intent);
+            }
+        });
     }
 }
